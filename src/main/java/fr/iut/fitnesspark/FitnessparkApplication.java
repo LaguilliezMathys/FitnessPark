684e0569a521b6068df19122f1b84a238d17f724
Laguilliez <laguilliezmathys1@gmail.com>
2026-02-11 10:32:42 +0100

    initialisation du répertoire

---
+++ src/main/java/fr/iut/fitnesspark/FitnessparkApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.fitnesspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/iut/fitnesspark/FitnessparkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.fitnesspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
